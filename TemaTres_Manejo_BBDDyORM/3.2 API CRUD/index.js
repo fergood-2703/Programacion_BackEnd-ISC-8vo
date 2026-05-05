c3eb38a2573ee3ef346934279152d5077ccd6edb
FerGood <omi.omar.2703@gmail.com>
2026-05-05 08:27:21 -0500

    Concluyendo  Actividad AA 3.2 Proyecto CRUD API

--- TemaTres_Manejo_BBDDyORM/3.2 API CRUD/index.js
+++ TemaTres_Manejo_BBDDyORM/3.2 API CRUD/index.js
@@ -39,3 +39,61 @@ app.post('/usuarios', async (req, res) => { //
     res.status(500).json({ error: 'Error al crear el usuario' });
   }
 });
+
+app.get('/usuarios', async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    res.status(500).json({ error: 'Error al obtener los usuarios' });
+  }
+})
+
+app.get('/usuario/:id', async (req, res) => {
+  try {
+    const { id } = req.params; // Extrae el ID de los parámetros de la solicitud
+    const usuario = await Usuario.findById(id);
+    res.status(200).json(usuario);
+  } catch (error) {
+    console.error("Error al obtener el usuario:", error);
+    res.status(500).json({ error: 'Error al obtener el usuario' });
+  }
+});
+
+app.put('/usuario/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+
+    if (!usuario) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    const usuarioActualizado = await Usuario.findById(id);
+
+    res.status(200).json(usuarioActualizado);
+    console.log(usuarioActualizado);
+
+  } catch (error) {
+    console.error("Error al actualizar el usuario:", error);
+    res.status(500).json({ error: 'Error al actualizar el usuario' });  
+  }
+});
+
+app.delete('/usuario/:id', async (req, res) => {
+  try{
+    const { id } = req.params;
+    const usuario = await Usuario.findByIdAndDelete(id);
+
+    if(!usuario){
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    res.status(200).json({ message: 'Usuario eliminado' });
+
+  }catch(error){
+    console.error("Error al eliminar el usuario:", error);
+    res.status(500).json({ error: 'Error al eliminar el usuario' });
+  }
+});
